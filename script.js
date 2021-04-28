ac7837985ea558219f5e12482940feb5d7aa9d0f
Nithi Pramesti <nithipramesti1508@gmail.com>
2021-04-28 21:13:12 +0800

    services button hover and click, click still need twice

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const menuServices = document.querySelector('#services');
 const menuExpandCont = document.querySelector('.menu-expand-container');
 menuExpandCont.style.display = 'none';
 
-menuServices.addEventListener('click', function(){
+// menuServices.addEventListener('click', function(){
+    // if(menuExpandCont.style.display === 'none') {
+    //     menuExpandCont.style.display = 'block';
+    // } else {
+    //     menuExpandCont.style.display = 'none';
+    // }
+// })
+
+(function() {
+    menuServices.addEventListener("click", expandMenu, false);
+    menuServices.addEventListener("mouseover", expandMenu, false);
+})();
+
+function expandMenu(e){
+    // e.preventDefault();
+
     if(menuExpandCont.style.display === 'none') {
         menuExpandCont.style.display = 'block';
     } else {
         menuExpandCont.style.display = 'none';
     }
-})
+}
