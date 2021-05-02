c87b42a5c72e2b8f4318045a0d51ddb2838d1703
Nithi Pramesti <nithipramesti1508@gmail.com>
2021-05-02 22:13:30 +0800

    menu mobile icon working, show and hide

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 //CONST & VAR
 const menuServices = document.querySelector('#services');
 const menuExpandCont = document.querySelector('.menu-expand-container');
+const menuContainer = document.querySelector('.menu-container');
+const menuMobile = document.querySelector('.menu-mobile')
+// const menuIcon = document.querySelector('.menu-icon');
+// const closeIcon = document.querySelector('.close-icon');
+
 menuExpandCont.style.display = 'none';
+menuContainer.style.display = 'none';
 
-// menuServices.addEventListener('click', function(){
-    // if(menuExpandCont.style.display === 'none') {
-    //     menuExpandCont.style.display = 'block';
-    // } else {
-    //     menuExpandCont.style.display = 'none';
-    // }
-// })
-
-(function() {
-    menuServices.addEventListener("click", expandMenu, false);
-    // menuServices.addEventListener("mouseover", expandMenu, false);
-    // menuExpandCont.addEventListener("mouseover", expandMenu, false);
-})();
-
-function expandMenu(e){
-    // e.preventDefault();
-
+menuServices.addEventListener('click', function(){
     if(menuExpandCont.style.display === 'none') {
         menuExpandCont.style.display = 'block';
     } else {
         menuExpandCont.style.display = 'none';
     }
-}
+})
+
+
+//MOBILE MENU ICON
+menuMobile.addEventListener('click', function() {
+    menuMobile.classList.toggle("close-icon");
+    menuMobile.classList.toggle("menu-icon");
+
+    if(menuContainer.style.display === 'none') {
+        menuContainer.style.display = 'block';
+    } else {
+        menuContainer.style.display = 'none';
+    }
+})
